cc2fd460f3b39d598b03aae1458864a96a537b64
karabaevas <karabaevmb@gmail.com>
2023-02-12 15:07:06 +0300

    add lombok, and removed boilerplate

--- src/main/java/com/betoola/homework/entity/RateId.java
+++ src/main/java/com/betoola/homework/entity/RateId.java
@@ -1,48 +1,17 @@
 package com.betoola.homework.entity;
 
-import javax.persistence.IdClass;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
 import java.io.Serializable;
-import java.util.Objects;
 
+@RequiredArgsConstructor
+@EqualsAndHashCode
+@Getter
+@Setter
 public class RateId implements Serializable {
-    private String fromCurrency;
-    private String toCurrency;
-
-    // default constructor
-
-    public RateId() {
-    }
-
-    public RateId(String fromCurrency, String toCurrency) {
-        this.fromCurrency = fromCurrency;
-        this.toCurrency = toCurrency;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof RateId rateId)) return false;
-        return Objects.equals(fromCurrency, rateId.fromCurrency) && Objects.equals(toCurrency, rateId.toCurrency);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(fromCurrency, toCurrency);
-    }
-
-    public String getFromCurrency() {
-        return fromCurrency;
-    }
-
-    public void setFromCurrency(String fromCurrency) {
-        this.fromCurrency = fromCurrency;
-    }
-
-    public String getToCurrency() {
-        return toCurrency;
-    }
-
-    public void setToCurrency(String toCurrency) {
-        this.toCurrency = toCurrency;
-    }
+    private final String fromCurrency;
+    private final String toCurrency;
 }
